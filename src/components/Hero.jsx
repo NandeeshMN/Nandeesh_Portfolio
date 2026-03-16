7fa62958321b8b9058242faa068f0f4bb574a819
Nandeesh <nandeeshmn.mca@gmail.com>
2026-03-16 19:16:10 +0530

    Fix responsive profile image circle distortion on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -126,9 +126,9 @@ const Hero = () => {
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1, delay: 0.5 }}
-            className="flex justify-center items-center order-1 lg:order-2 h-[300px] md:h-[400px] lg:h-[500px]"
+            className="flex justify-center items-center order-1 lg:order-2 w-full"
           >
-            <div className="relative w-full h-full max-h-[500px] max-w-[500px] rounded-full p-2 border-2 border-black/10 dark:border-white/10 shadow-2xl">
+            <div className="relative w-full max-w-[280px] sm:max-w-[350px] md:max-w-[400px] lg:max-w-[480px] aspect-square rounded-full p-2 border-2 border-black/10 dark:border-white/10 shadow-2xl">
               <div className="w-full h-full rounded-full overflow-hidden bg-darker shadow-inner">
                 <img 
                   src={MyProfile} 
