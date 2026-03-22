08aa63c54dfd749729e98bd65e8fe4d3aa32015e
Nandeesh <nandeeshmn.mca@gmail.com>
2026-03-22 23:16:19 +0530

    Adjust profile image positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -133,7 +133,7 @@ const Hero = () => {
                 <img 
                   src={MyProfile} 
                   alt={portfolioData.personalInfo.name} 
-                  className="w-full h-full object-cover object-center rounded-full"
+                  className="w-full h-full object-cover object-[50%_10%] rounded-full"
                 />
               </div>
             </div>
